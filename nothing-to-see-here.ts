e64d518e9bccc7b7bba0d833382fef7a854a58a6
`${f1f47∀23}` <f1f47a23@mail.com>
2022-09-18 10:01:48 +0200

    Update nothing-to-see-here.ts

--- nothing-to-see-here.ts
+++ nothing-to-see-here.ts
@@ -3,7 +3,7 @@
 // BEGINOF DUMMY CODE
 //
 //      Nothing to see here, move along
-//      just to tease the github search engine
+//      this file is here just to tease the github search engine
 //
 
 export { CDummy };
